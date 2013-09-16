7e8714af6448e4d75a91b8c7713228d2282db6ea
Данилов Александр <snaiffer@snaifferPC.(none)>
2013-09-16 08:43:34 +0400

    .vsd file was added; the extension was changed from .s to .asm

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-numb: numb.s
-	nasm -f elf numb.s 
+numb: numb.asm
+	nasm -f elf numb.asm 
 	ld -m elf_i386 -s -o numb numb.o
 	rm numb.o
--- numb.asm
+++ numb.asm
@@ -77,37 +77,37 @@ section	.text
 
 	 L2:
 		pusha
-		
+
 		 mov	eax, SYS_WRITE
 		 mov	ebx, STDOUT
 		 mov	ecx, msg_invit1
 		 mov	edx, msg_invit1_len
 		 int	0x80
-	
+
 		 mov al, [i]
 		 call print_num
-	
+
 		 mov	eax, SYS_WRITE
 		 mov	ebx, STDOUT
 		 mov	ecx, msg_invit2
 		 mov	edx, msg_invit2_len
 		 int	0x80
-	
+
 		 mov al, [j]
 		 call print_num
-	
+
 		 mov	eax, SYS_WRITE
 		 mov	ebx, STDOUT
 		 mov	ecx, msg_invit3
 		 mov	edx, msg_invit3_len
 		 int	0x80
-	
+
 		 call read_num
 		 mov [esi+2], al
-	
+
 		 mov al, [num]
 		 call print_num
-	
+
 		popa
 	 dec cl
 	 JNZ L2
